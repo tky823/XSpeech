1525f173eb49f2e5915ba2851fa55e36b9c07589
tky823 <tky823.main+github@gmail.com>
2022-06-30 22:40:19 +0900

    [src] Add conditioned module

--- xspeech/models/tdcn.py
+++ xspeech/models/tdcn.py
@@ -40,6 +40,97 @@ class DepthwiseSeparableConv1d(nn.Module):
             dilation=dilation,
             groups=in_channels,
         )
+
+        if nonlinear is not None:
+            if nonlinear == "prelu":
+                self.nonlinear1d = nn.PReLU()
+            else:
+                raise ValueError("Not support {}".format(nonlinear))
+
+            self.nonlinear = True
+        else:
+            self.nonlinear = False
+
+        if norm:
+            norm_name = "cLN" if causal else "gLN"
+            self.norm1d = choose_layer_norm(
+                norm_name, in_channels, causal=causal, eps=eps
+            )
+
+        if dual_head:
+            self.output_pointwise_conv1d = nn.Conv1d(
+                in_channels, out_channels, kernel_size=1, stride=1
+            )
+
+        self.skip_pointwise_conv1d = nn.Conv1d(
+            in_channels, skip_channels, kernel_size=1, stride=1
+        )
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        r"""
+        Args:
+            input (torch.Tensor):
+                Input tensor with shape of (batch_size, in_channels, num_samples).
+
+        Returns:
+            torch.Tensor:
+                Output tensor with shape of (batch_size, in_channels, num_samples).
+            torch.Tensor:
+                Output tensor for skip connection.
+                The shape is (batch_size, in_channels, num_samples).
+        """
+        nonlinear, norm = self.nonlinear, self.norm
+        dual_head = self.dual_head
+
+        x = self.depthwise_conv1d(input)
+
+        if nonlinear:
+            x = self.nonlinear1d(x)
+
+        if norm:
+            x = self.norm1d(x)
+
+        if dual_head:
+            output = self.output_pointwise_conv1d(x)
+        else:
+            output = None
+
+        skip = self.skip_pointwise_conv1d(x)
+
+        return output, skip
+
+
+class ConditionedDepthwiseSeparableConv1d(nn.Module):
+    r"""Depthwise separable 1D convolution using FiLM."""
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int = 256,
+        skip_channels: int = 256,
+        kernel_size: int = 3,
+        stride: int = 2,
+        dilation: int = 1,
+        causal: bool = False,
+        nonlinear: Optional[str] = None,
+        norm: bool = True,
+        dual_head: bool = True,
+        eps: float = EPS,
+    ) -> None:
+        super().__init__()
+
+        self.dual_head = dual_head
+        self.norm = norm
+        self.eps = eps
+
+        self.depthwise_conv1d = nn.Conv1d(
+            in_channels,
+            in_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            dilation=dilation,
+            groups=in_channels,
+        )
         self.conditioning = FiLM1d()
 
         if nonlinear is not None:
@@ -47,6 +138,7 @@ class DepthwiseSeparableConv1d(nn.Module):
                 self.nonlinear1d = nn.PReLU()
             else:
                 raise ValueError("Not support {}".format(nonlinear))
+
             self.nonlinear = True
         else:
             self.nonlinear = False
@@ -73,6 +165,11 @@ class DepthwiseSeparableConv1d(nn.Module):
         Args:
             input (torch.Tensor):
                 Input tensor with shape of (batch_size, in_channels, num_samples).
+            scale (torch.Tensor):
+                Conditioning tensor with shape of (batch_size, in_channels).
+            bias (torch.Tensor):
+                Conditioning tensor with shape of (batch_size, in_channels).
+
         Returns:
             torch.Tensor:
                 Output tensor with shape of (batch_size, in_channels, num_samples).
