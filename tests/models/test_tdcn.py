870017fe641c5ff2cfe77bc8c5324204a73b8cf2
tky823 <tky823.main+github@gmail.com>
2022-07-01 09:10:49 +0900

    [tests] Add tests for residual blocks

--- tests/models/test_tdcn.py
+++ tests/models/test_tdcn.py
@@ -3,14 +3,94 @@ import torch
 import torch.nn.functional as F
 
 from xspeech.models.tdcn import (
+    ResidualBlock1d,
+    ConditionedResidualBlock1d,
     DepthwiseSeparableConv1d,
     ConditionedDepthwiseSeparableConv1d,
 )
 
+parameters_residual_block1d = [
+    (4, 3, 5, 16, 3, 1, 1, 10),
+    (2, 3, 6, 4, 3, 1, 2, 16),
+]
 parameters_depthwise_separable_conv1d = [
     (4, 3, 16, 12, 3, 1, 1, 10),
     (2, 3, 4, 5, 3, 1, 2, 16),
 ]
+
+
+@pytest.mark.parametrize(
+    "batch_size, in_channels, hidden_channels, skip_channels, \
+        kernel_size, stride, dilation, num_samples",
+    parameters_residual_block1d,
+)
+def test_residual_block1d(
+    batch_size: int,
+    in_channels: int,
+    hidden_channels: int,
+    skip_channels: int,
+    kernel_size: int,
+    stride: int,
+    dilation: int,
+    num_samples: int,
+):
+    model = ResidualBlock1d(
+        in_channels,
+        hidden_channels=hidden_channels,
+        skip_channels=skip_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        dilation=dilation,
+        causal=False,
+        norm=True,
+        dual_head=True,
+    )
+
+    input = torch.randn(batch_size, in_channels, num_samples)
+
+    with torch.no_grad():
+        output, skip = model(input)
+
+    assert output.size() == (batch_size, in_channels, num_samples)
+    assert skip.size() == (batch_size, skip_channels, num_samples)
+
+
+@pytest.mark.parametrize(
+    "batch_size, in_channels, hidden_channels, skip_channels, \
+        kernel_size, stride, dilation, num_samples",
+    parameters_residual_block1d,
+)
+def test_conditioned_residual_block(
+    batch_size: int,
+    in_channels: int,
+    hidden_channels: int,
+    skip_channels: int,
+    kernel_size: int,
+    stride: int,
+    dilation: int,
+    num_samples: int,
+):
+    model = ConditionedResidualBlock1d(
+        in_channels,
+        hidden_channels=hidden_channels,
+        skip_channels=skip_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        dilation=dilation,
+        causal=False,
+        norm=True,
+        dual_head=True,
+    )
+
+    input = torch.randn(batch_size, in_channels, num_samples)
+    scale = torch.randn(batch_size, hidden_channels)
+    bias = torch.randn(batch_size, hidden_channels)
+
+    with torch.no_grad():
+        output, skip = model(input, scale=scale, bias=bias)
+
+    assert output.size() == (batch_size, in_channels, num_samples)
+    assert skip.size() == (batch_size, skip_channels, num_samples)
 
 
 @pytest.mark.parametrize(
